94e2cf01c535f66fbf2bdd0bbe78135a8fb53f3c
arraf2211 <arrafrahman22@gmail.com>
2022-10-02 22:16:12 +0100

    add value exception

--- primes.py
+++ primes.py
@@ -3,4 +3,20 @@
 
 def primes(number_of_primes):
     list = []
+    numbers = 3
+    if number_of_primes > 0:
+        list.append(2)
+        while len(list) < number_of_primes:
+            flag = False
+            for i in range(2, numbers):
+                if numbers % i == 0:
+                    flag = False
+                    break
+                else:
+                    flag = True
+            if flag:
+                list.append(numbers)
+            numbers = numbers + 1
+     else:
+         raise ValueError()
     return list
